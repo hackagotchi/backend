6fc397e478adc9981f7af0916e1538a496927655
Owen Salter <owen@devosmium.xyz>
2020-06-18 18:34:04 -0400

    Add basic unimplemented route to main.rs
    Needs to be factored into a routes module and implemented properly

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use actix_web::{get, App, HttpServer, HttpResponse, HttpRequest, web};
+
+#[get("/user/{id}")]
+async fn get_user(_req: HttpRequest) -> HttpResponse {
+    /*!
+     * Gets a user from the API
+     */
+
+    HttpResponse::Ok().body("success")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    
+    HttpServer::new(move || {
+        App::new()
+            .service(get_user)
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
