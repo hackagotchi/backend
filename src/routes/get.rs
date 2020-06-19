6463737e35572d622f05be92dc035a8e118b34dc
Cedric Hutchings <cedhut02@gmail.com>
2020-06-18 23:49:52 -0400

    Handle errors more concisely

--- src/routes/get.rs
+++ src/routes/get.rs
@@ -5,30 +5,17 @@ use crate::models::{User, UserContact};
 
 #[get("/user/")]
 pub async fn get_user(form: web::Form<User>) -> Result<HttpResponse, ServiceError> {
-
     let user_id = form.id;
     let db = get_mongo_database().await?;
     let collection = db.collection("users");
     let filter = doc! {"id": user_id.to_string()};
-    let result = match collection.find_one(filter, None).await {
-        Ok(opt) => match opt {
-            Some(doc) => doc,
-            None => {
-                return Err(ServiceError::NoData);
-            }
-        },
-        Err(err) => {
-            return Err(err.into());
-        }
-    };
-    let res_user_id = user_id;
-    let res_contact = form.contact.clone();
+    let result = collection
+        .find_one(filter, None)
+        .await?
+        .ok_or(ServiceError::NoData)?;
 
-    let json = web::Json(User {
-        id: res_user_id,
-        contact: res_contact,
-    });
-
-
-    Ok(HttpResponse::Ok().json(json.0))
+    Ok(HttpResponse::Ok().json(User {
+        id: user_id,
+        contact: form.contact.clone(),
+    }))
 }
